b9267ee966adcb49a142427a9ba0207fd35f9a63
JohnMarquez <35536915+JohnMarquez@users.noreply.github.com>
2020-01-13 18:18:10 -0600

    code challenge finally added to right repo pretty late

---
+++ Library/LibraryPal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class LibraryPal
+    {
+        public string Lower(string pal)
+        {
+            pal = pal.ToLower();
+            return pal;
+        }
+
+        public string charRemoval(string pal)
+        {
+            pal = pal.Replace(" ", String.Empty);
+            pal = pal.Replace(".", String.Empty);
+            pal = pal.Replace(",", String.Empty);
+            return pal;
+        }
+        public bool Palindrone(string pal)
+        {
+            int length = pal.Length;
+            int j = pal.Length - 1;
+            char[] arr;
+            arr = pal.ToCharArray();
+            bool check = true;
+
+            for (int i = 0; i < length-1; i++)
+            {
+                if (arr[i] != arr[j])
+                {
+                    Console.WriteLine("Not a Palandrone");
+                    check = false;
+                    break;
+                }
+                j--;
+            }
+
+            Console.WriteLine("Correct! That is a Palindrone");
+            return check;
+        }
+
+
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please type in a Palandrone");
+            string pal = Console.ReadLine();
+
+            LibraryPal lib = new LibraryPal();
+            pal = lib.Lower(pal);
+            pal = lib.charRemoval(pal);
+            //pal = pal.ToLower();
+            //pal = pal.Replace(" ", String.Empty);
+            //pal = pal.Replace(".", String.Empty);
+            //pal = pal.Replace(",", String.Empty);
+
+            //Console.WriteLine(pal);
+            //LibraryPal lib = new LibraryPal();
+            lib.Palindrone(pal);
+            
+        }
+    }
+}
